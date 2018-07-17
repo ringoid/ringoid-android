d6712ee54c4cfe8dd22c126cce086073b3a94acf
Alexandr Dev <blackdizel@gmail.com>
2018-07-17 21:40:59 +0300

    misc

--- app/src/main/java/org/byters/ringoid/view/NavigatorPages.java
+++ app/src/main/java/org/byters/ringoid/view/NavigatorPages.java
@@ -2,6 +2,12 @@ package org.byters.ringoid.view;
 
 import android.support.v4.app.FragmentManager;
 
+import org.byters.ringoid.view.ui.fragment.FragmentExplore;
+import org.byters.ringoid.view.ui.fragment.FragmentLikes;
+import org.byters.ringoid.view.ui.fragment.FragmentMessages;
+import org.byters.ringoid.view.ui.fragment.FragmentProfile;
+import org.byters.ringoid.view.ui.fragment.FragmentRank;
+
 import java.lang.ref.WeakReference;
 
 public class NavigatorPages implements INavigatorPages {
@@ -17,26 +23,46 @@ public class NavigatorPages implements INavigatorPages {
 
     @Override
     public void navigateRank() {
-
+        if (refFragmentManager == null || refFragmentManager.get() == null) return;
+        refFragmentManager.get()
+                .beginTransaction()
+                .replace(viewId, new FragmentRank())
+                .commit();
     }
 
     @Override
     public void navigateLikes() {
-
+        if (refFragmentManager == null || refFragmentManager.get() == null) return;
+        refFragmentManager.get()
+                .beginTransaction()
+                .replace(viewId, new FragmentLikes())
+                .commit();
     }
 
     @Override
     public void navigateProfile() {
-
+        if (refFragmentManager == null || refFragmentManager.get() == null) return;
+        refFragmentManager.get()
+                .beginTransaction()
+                .replace(viewId, new FragmentProfile())
+                .commit();
     }
 
     @Override
     public void navigateMessages() {
-
+        if (refFragmentManager == null || refFragmentManager.get() == null) return;
+        refFragmentManager.get()
+                .beginTransaction()
+                .replace(viewId, new FragmentMessages())
+                .commit();
     }
 
     @Override
     public void navigateExplore() {
-
+        if (refFragmentManager == null || refFragmentManager.get() == null) return;
+        refFragmentManager.get()
+                .beginTransaction()
+                .replace(viewId, new FragmentExplore())
+                .commit();
     }
 }
