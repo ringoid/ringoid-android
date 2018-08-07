989358df0a1b4664c7749a9f9c26d5d995399965
blackdizel <blackdizel@gmail.com>
2018-08-07 12:41:18 +0300

    terms clickable links

--- app/src/main/java/org/byters/ringoid/view/ui/fragment/FragmentLogin.java
+++ app/src/main/java/org/byters/ringoid/view/ui/fragment/FragmentLogin.java
@@ -3,6 +3,7 @@ package org.byters.ringoid.view.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,6 +65,8 @@ public class FragmentLogin extends FragmentBase
         etPhone = view.findViewById(R.id.etPhone);
         etCodeSMS = view.findViewById(R.id.etCodeSMS);
         cbAge = view.findViewById(R.id.cbAge);
+
+        cbTerms.setMovementMethod(new LinkMovementMethod());
 
         view.findViewById(R.id.tvRegister).setOnClickListener(this);
         view.findViewById(R.id.tvSexFemale).setOnClickListener(this);
