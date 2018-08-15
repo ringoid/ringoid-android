64100959a97d8466f6c42f1e68cd3935d4c86ab7
blackdizel <blackdizel@gmail.com>
2018-08-15 15:16:36 +0300

    edit parse phone method

--- ccp/src/main/java/com/hbb20/PhoneUtils.java
+++ ccp/src/main/java/com/hbb20/PhoneUtils.java
@@ -1,6 +1,7 @@
 package com.hbb20;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
@@ -10,8 +11,9 @@ import com.google.i18n.phonenumbers.Phonenumber;
 public class PhoneUtils {
 
     public static String getPhone(Context context, String data) {
+        if (TextUtils.isEmpty(data)) return null;
         Phonenumber.PhoneNumber number = getNumber(context, data);
-        return number == null ? data : String.valueOf(number.getNationalNumber());
+        return number == null ? data.replaceAll("[^0-9]", "") : String.valueOf(number.getNationalNumber());
     }
 
     public static String getCode(Context context, String data) {
