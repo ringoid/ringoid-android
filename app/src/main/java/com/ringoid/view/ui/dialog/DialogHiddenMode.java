b065ef13db6337d3eeb05242374df0cc0631dcbf
blackdizel <blackdizel@gmail.com>
2018-08-22 15:24:30 +0300

    add dialog hidden mode

---
+++ app/src/main/java/com/ringoid/view/ui/dialog/DialogHiddenMode.java
@@ -0,0 +1,60 @@
+/*Copyright (c) Ringoid Ltd, 2018. All Rights Reserved*/
+
+
+package com.ringoid.view.ui.dialog;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+
+import com.ringoid.R;
+import com.ringoid.view.ui.dialog.callback.IDialogHiddenModeListener;
+import com.ringoid.view.ui.dialog.callback.IDialogWhoLikedYouListener;
+
+public class DialogHiddenMode implements View.OnClickListener {
+
+
+    private IDialogHiddenModeListener listener;
+    private AlertDialog dialog;
+    private CheckBox cbDontShow;
+
+    public DialogHiddenMode(Context context, IDialogHiddenModeListener listenerDialogProfileLikes) {
+        this.listener = listenerDialogProfileLikes;
+        initDialog(context);
+    }
+
+    private void initDialog(Context context) {
+        dialog = new AlertDialog.Builder(context).create();
+        View view = LayoutInflater.from(context).inflate(R.layout.view_dialog_hidden_mode, null);
+        dialog.setView(view);
+
+        view.findViewById(R.id.tvAbout).setOnClickListener(this);
+        view.findViewById(R.id.tvPrivacy).setOnClickListener(this);
+        view.findViewById(R.id.tvOK).setOnClickListener(this);
+        cbDontShow = view.findViewById(R.id.cbShowAgain);
+    }
+
+    public void cancel() {
+        dialog.cancel();
+    }
+
+    public void show() {
+        dialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tvAbout)
+            listener.onSelectAbout(!cbDontShow.isChecked());
+
+        if (v.getId() == R.id.tvPrivacy)
+            listener.onSelectPrivacy(!cbDontShow.isChecked());
+
+        if (v.getId() == R.id.tvOK)
+            listener.onSelectOK(!cbDontShow.isChecked());
+
+        cancel();
+    }
+}
---
+++ app/src/main/java/com/ringoid/view/ui/dialog/callback/IDialogHiddenModeListener.java
@@ -0,0 +1,10 @@
+package com.ringoid.view.ui.dialog.callback;
+/*Copyright (c) Ringoid Ltd, 2018. All Rights Reserved*/
+
+public interface IDialogHiddenModeListener {
+    void onSelectAbout(boolean b);
+
+    void onSelectOK(boolean b);
+
+    void onSelectPrivacy(boolean b);
+}
