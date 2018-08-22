cc2e75ac5c64c844df9be6b5f9dbf51fa3e35291
blackdizel <blackdizel@gmail.com>
2018-08-22 15:15:07 +0300

    add dialog new faces

---
+++ app/src/main/java/com/ringoid/view/ui/dialog/callback/IDialogNewFacesListener.java
@@ -0,0 +1,10 @@
+package com.ringoid.view.ui.dialog.callback;
+/*Copyright (c) Ringoid Ltd, 2018. All Rights Reserved*/
+
+public interface IDialogNewFacesListener {
+    void onSelectAbout(boolean b);
+
+    void onSelectPush(boolean b);
+
+    void onSelectOK(boolean b);
+}
