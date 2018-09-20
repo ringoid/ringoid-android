bc96c683fe74064c49d8e322a2369d280e3fce59
blackdizel <blackdizel@gmail.com>
2018-09-20 12:17:14 +0300

    add dialog privacy likes

---
+++ app/src/main/java/com/ringoid/view/ui/dialog/callback/IDialogPrivacyLikesListener.java
@@ -0,0 +1,8 @@
+package com.ringoid.view.ui.dialog.callback;
+/*Copyright (c) Ringoid Ltd, 2018. All Rights Reserved*/
+
+public interface IDialogPrivacyLikesListener {
+    void onClickMatches();
+
+    void onClickLiked();
+}
