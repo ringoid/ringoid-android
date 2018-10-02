a483822a6108b30b36a355e8c6fbc555cea867b9
blackdizel <blackdizel@gmail.com>
2018-10-02 14:50:59 +0300

    edit messages tab item click

--- app/src/main/java/com/ringoid/view/presenter/PresenterAdapterMessagesImages.java
+++ app/src/main/java/com/ringoid/view/presenter/PresenterAdapterMessagesImages.java
@@ -2,8 +2,13 @@
 package com.ringoid.view.presenter;
 
 import com.ringoid.ApplicationRingoid;
+import com.ringoid.R;
+import com.ringoid.controller.data.memorycache.ICacheChatMessages;
 import com.ringoid.controller.data.memorycache.ICacheMessages;
 import com.ringoid.view.INavigator;
+import com.ringoid.view.IViewDialogs;
+import com.ringoid.view.IViewPopup;
+import com.ringoid.view.ui.dialog.callback.IDialogChatComposeListener;
 
 import javax.inject.Inject;
 
@@ -14,6 +19,17 @@ public class PresenterAdapterMessagesImages implements IPresenterAdapterMessages
 
     @Inject
     INavigator navigator;
+
+    @Inject
+    IViewDialogs viewDialogs;
+
+    @Inject
+    ICacheChatMessages cacheChatMessages;
+
+    @Inject
+    IViewPopup viewPopup;
+
+    private ListenerDialogChatCompose listenerDialogChatCompose;
 
     public PresenterAdapterMessagesImages() {
         ApplicationRingoid.getComponent().inject(this);
@@ -32,6 +48,28 @@ public class PresenterAdapterMessagesImages implements IPresenterAdapterMessages
     @Override
     public void onClickItem(int position) {
         cacheMessages.setUserSelected(position);
+
+        boolean isChatEmpty = !cacheChatMessages.isDataExist(cacheMessages.getUserId(position));;
+
+        if (isChatEmpty) {
+            viewDialogs.showDialogChatCompose(listenerDialogChatCompose = new ListenerDialogChatCompose(cacheMessages.getUserId(position)));
+            return;
+        }
+
         navigator.navigateChat();
+    }
+
+    private class ListenerDialogChatCompose implements IDialogChatComposeListener {
+        private String userId;
+
+        ListenerDialogChatCompose(String userId) {
+            this.userId = userId;
+        }
+
+        @Override
+        public void onSend(String message) {
+            viewPopup.showToast(R.string.message_sent);
+            cacheChatMessages.addMessage(userId, message);
+        }
     }
 }
