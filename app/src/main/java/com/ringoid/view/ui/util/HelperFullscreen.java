18dd5c61880016c19a504d58d3b4d0ddba738147
blackdizel <blackdizel@gmail.com>
2018-10-11 17:24:42 +0300

    remove flag hide navigation

--- app/src/main/java/com/ringoid/view/ui/util/HelperFullscreen.java
+++ app/src/main/java/com/ringoid/view/ui/util/HelperFullscreen.java
@@ -37,7 +37,6 @@ public class HelperFullscreen implements IHelperFullscreen {
 
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
         decorView.setSystemUiVisibility(uiOptions);
 
