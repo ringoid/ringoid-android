f476fd0f8241f1d934d740ebbfcfbacccc048c32
Alexandr Dev <blackdizel@gmail.com>
2018-10-12 15:03:36 +0300

    add empty matches

--- app/src/main/java/com/ringoid/view/ui/fragment/FragmentMatches.java
+++ app/src/main/java/com/ringoid/view/ui/fragment/FragmentMatches.java
@@ -1,10 +1,26 @@
 package com.ringoid.view.ui.fragment;
 /*Copyright (c) Ringoid Ltd, 2018. All Rights Reserved*/
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ringoid.R;
 import com.ringoid.view.PAGE_ENUM;
 
 public class FragmentMatches extends FragmentFeedPage {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container);
+        initViews(view);
+        showEmpty(R.string.message_empty_matches);
+        return view;
+    }
 
     @Override
     public PAGE_ENUM getPage() {
