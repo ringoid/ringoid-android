e2e0d94883d61b66c1c47ce0c01cf04ecc977a18
blackdizel <blackdizel@gmail.com>
2018-10-15 13:01:35 +0300

    edit dialog colors

--- app/src/main/java/com/ringoid/view/ui/dialog/DialogReport.java
+++ app/src/main/java/com/ringoid/view/ui/dialog/DialogReport.java
@@ -3,7 +3,6 @@ package com.ringoid.view.ui.dialog;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -14,6 +13,7 @@ import com.ringoid.R;
 
 public class DialogReport implements View.OnClickListener {
 
+    private TextView tvReport;
     private AlertDialog dialog;
 
     public DialogReport(Context context) {
@@ -21,7 +21,8 @@ public class DialogReport implements View.OnClickListener {
         View view = LayoutInflater.from(context).inflate(R.layout.view_dialog_report, null);
         dialog.setView(view);
 
-        view.findViewById(R.id.tvReport).setOnClickListener(this);
+        tvReport = view.findViewById(R.id.tvReport);
+        tvReport.setOnClickListener(this);
         view.findViewById(R.id.tvCancel).setOnClickListener(this);
 
         ((Switch) view.findViewById(R.id.swReport)).setOnCheckedChangeListener(new ListenerSwitch());
@@ -44,7 +45,7 @@ public class DialogReport implements View.OnClickListener {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             dialog.findViewById(R.id.rgReport).setVisibility(isChecked ? View.VISIBLE : View.GONE);
-
+            tvReport.setTextColor(dialog.getContext().getResources().getColor(isChecked ? R.color.colorAccent : android.R.color.white));
             ((TextView) dialog.findViewById(R.id.tvReport)).setText(isChecked ? R.string.dialog_report_confirm_report : R.string.dialog_report_confirm);
         }
     }
